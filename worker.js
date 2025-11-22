7a4beaf780d9318552ab9325c8fd21beba6c0d39
Xuanqiang Liao <44645621+liaoxuanqiang@users.noreply.github.com>
2025-11-22 20:05:12 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
+const BOT_TOKEN = "8469649001:AAEuoN6d9gEN-z7Xwh927l9cZQxVffy9zao"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
-const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
-const BOT_OWNER = 123456789; // Insert your telegram account id.
-const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
-const SIA_SECRET = "SIA_SECRET"; // Insert a powerful secret text and keep it safe.
+const BOT_SECRET = "password"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
+const BOT_OWNER = 5330543449; // Insert your telegram account id.
+const BOT_CHANNEL = -1003453224926; // Insert your telegram channel id which the bot is admin in.
+const SIA_SECRET = "659c6d3f-08cc-4e13-8b52-55b8c3498ce9"; // Insert a powerful secret text and keep it safe.
 const PUBLIC_BOT = false; // Make your bot public (only [true, false] are allowed).
 
 // ---------- Do Not Modify ---------- // 
@@ -21,7 +21,7 @@ const ERROR_408 = {"ok":false,"error_code":408,"description":"Bad Request: mode 
 
 // ---------- Event Listener ---------- // 
 
-addEventListener('fetch', event => {
+addEventListener('fetch'， event => {
     event.respondWith(handleRequest(event))
 });
 
